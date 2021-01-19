014994e753ce83cd280181bbbf4330a92fac4a9b
authentic-pipe-8414 <tasher.stidham@outlook.com>
2021-01-19 17:23:03 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,8 @@
 def square_array(array)
   new_array = []
   array.each{ |square| new_array << square ** 2} 
+<<<<<<< HEAD
   new_array
+=======
+>>>>>>> 1b3517bc2b2ac3a4d83380c8c3925a87ef9c6566
 end
